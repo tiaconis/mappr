026f9004bddd46463f8c3794ef3bd5e4849aca9e
Tiffany Iaconis <tiaconis@Tiffanys-MacBook-Pro.local>
2013-11-17 15:56:25 -0500

    Added form for place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,6 +5,20 @@ class PlacesController < ApplicationController
 	end
 
 	def new
+		@place = Place.new
+		# @place = Place.last
 	end
+
+	def create
+		@place = Place.create( place_params )
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:title, :address)
+	end
+
 
 end
